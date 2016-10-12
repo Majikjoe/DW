2291081d354e77bd4e0672e50e902eabea6b6a42
Majikjoe <jonas.hp.nilsson@gmail.com>
2016-10-12 14:49:38 +0200

    Ändringar 12/10 kl14:49

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Created by Jonas on 2016-10-04.
  */
-
+/*
 $(document).ready(function(){
     $(".menu-link").hover(function() {
         $(this).css("color", "#0099ff");
@@ -9,13 +9,55 @@ $(document).ready(function(){
         $(this).css("color", "#595959");
     });
 });
-
+*/
 
 // Load the IFrame Player API code asynchronously.
+
+
+
+
+
+$(document).ready(function(){
+    var scroll_pos = 0;
+    $(document).scroll(function() {
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 450) {
+
+            $("header").css('background-color', '#060419');
+            $(".header ul li a").css('color', 'white');
+            $(".header h6").css('color', 'white');
+            $(".header p").css('color', 'white');
+
+            $(".menu-link").hover(function() {
+                $(this).css("color", "#a6a6a6");
+            }, function() {
+                $(this).css("color", "white");
+            });
+
+
+        } else {
+            $("header").css('background-color', 'white');
+            $(".header ul li a").css('color', '#595959');
+            $(".header h6").css('color', '#000');
+            $(".header p").css('color', '#000');
+
+            $(".menu-link").hover(function() {
+                $(this).css("color", "#a6a6a6");
+            }, function() {
+                $(this).css("color", "#000");
+            });
+
+
+        }
+    });
+});
+
+
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
@@ -29,13 +71,30 @@ function onYouTubePlayerAPIReady() {
             'controls': 0,
             'iv_load_policy': 3,
             'loop': 1,
+            'modestbranding': 1,
             'playlist': 'DdMtpHCZYU8'
+        },
+        events: {
+            'onReady': onPlayerReady
         }
+
     });
 }
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
+function onPlayerReady(event){
     player.mute();
 }
+
+
+$('.mute-toggle').on('click', function() {
+    var mute_toggle = $(this);
+
+    if(player.isMuted()){
+        player.unMute();
+        mute_toggle.text('volume_up');
+    }
+    else{
+        player.mute();
+        mute_toggle.text('volume_off');
+    }
+});
