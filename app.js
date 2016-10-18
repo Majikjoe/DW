f174e50d5cf1a634d0adf45b9b33aab409a0e2d2
Majikjoe <jonas.hp.nilsson@gmail.com>
2016-10-18 13:01:26 +0200

    18/10 kl13:01

--- app.js
+++ app.js
@@ -1,56 +1,6 @@
 /**
  * Created by Jonas on 2016-10-04.
  */
-/*
-$(document).ready(function(){
-    $(".menu-link").hover(function() {
-        $(this).css("color", "#0099ff");
-    }, function() {
-        $(this).css("color", "#595959");
-    });
-});
-*/
-
-// Load the IFrame Player API code asynchronously.
-
-
-
-
-
-$(document).ready(function(){
-    var scroll_pos = 0;
-    $(document).scroll(function() {
-        scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 450) {
-
-            $("header").css('background-color', '#060419');
-            $(".header ul li a").css('color', 'white');
-            $(".header h6").css('color', 'white');
-            $(".header p").css('color', 'white');
-
-            $(".menu-link").hover(function() {
-                $(this).css("color", "#a6a6a6");
-            }, function() {
-                $(this).css("color", "white");
-            });
-
-
-        } else {
-            $("header").css('background-color', 'white');
-            $(".header ul li a").css('color', '#595959');
-            $(".header h6").css('color', '#000');
-            $(".header p").css('color', '#000');
-
-            $(".menu-link").hover(function() {
-                $(this).css("color", "#a6a6a6");
-            }, function() {
-                $(this).css("color", "#000");
-            });
-
-
-        }
-    });
-});
 
 
 var tag = document.createElement('script');
@@ -85,3 +35,11 @@ function onPlayerReady(event){
     player.mute();
 }
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
